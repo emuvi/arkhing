84ec6f29b122f53c444a4cc380e0d75a2f23e8dc
Éverton M. Vieira <emuvi@outlook.com.br>
2024-02-11 15:45:13 -0300

    updates

--- src/main/java/br/com/pointel/arkhing/DeskDock.java
+++ src/main/java/br/com/pointel/arkhing/DeskDock.java
@@ -1,5 +1,8 @@
 package br.com.pointel.arkhing;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DeskDock extends javax.swing.JPanel {
 
     private final Desk desk;
@@ -13,19 +16,43 @@ public class DeskDock extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonFree = new javax.swing.JButton();
+
+        buttonFree.setText("Free");
+        buttonFree.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonFreeActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 418, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(buttonFree)
+                .addContainerGap(340, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 298, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(buttonFree)
+                .addContainerGap(269, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void buttonFreeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonFreeActionPerformed
+        try {
+            desk.getBase().arkhDocs.freeAll();
+        } catch (Exception ex) {
+            WizSwing.showError(ex);
+        }
+    }//GEN-LAST:event_buttonFreeActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton buttonFree;
     // End of variables declaration//GEN-END:variables
 }
